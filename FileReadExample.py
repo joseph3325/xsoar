5d2fe4fab5189b3ca87b5eaede0a1723df76223c
Joe <joseph3325@gmail.com>
2022-11-09 08:00:46 -0500

    Update FileReadExample.py

--- FileReadExample.py
+++ FileReadExample.py
@@ -1,9 +1,12 @@
+# Retrieve the argument 'entryId'
 entryId = demisto.args().get('entryId')
-
-
+# Function which takes the entryId for a json file as an arg and returns the json
 def get_file(entry_id):
     fp = demisto.executeCommand('getFilePath', {'id': entry_id})
     fp = fp[0]['Contents']['path']
     with open(fp, 'r') as f:
         data = json.loads(f.read())
     return data
+
+
+get_file(entryId)
